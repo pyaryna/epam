d528b05db72a724d1f414bf7ac7b34a29c6387c3
Yaryna <p.yaryna@gmail.com>
2019-11-06 20:10:23 +0200

    add threads homework

---
+++ threads/Matrix.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threads
+{
+    class Matrix
+    {
+        public int[,] Value { get; set; }
+        public int Rows { get; private set; }
+        public int Colums { get; private set; }
+
+        private int _sum;
+        private int[] _partSum;
+
+        public Matrix(){}
+        public Matrix(int n, int m)
+        {
+            Rows = n;
+            Colums = m;
+            Value = GetMatrix(n, m);
+            _partSum = new int[n];
+        }
+
+        public int AddElements(int amountOfThreads)
+        {
+            _sum = 0;            
+            int start;
+            int end;
+            amountOfThreads = amountOfThreads > Rows ? Rows : amountOfThreads;
+            Thread[] threads = new Thread[amountOfThreads];
+
+            int forThread = Rows / (amountOfThreads);           
+
+            for (int i = 0; i < amountOfThreads; i++)
+            {
+                if (i != amountOfThreads - 1)
+                {
+                    start = i * forThread;
+                    end = (i + 1) * forThread;
+                    threads[i] = new Thread(() => AddInRows(start, end));
+                    threads[i].Start();
+                }
+                else
+                {
+                    start = i * forThread;
+                    end = Rows;
+                        threads[i] = new Thread(() => AddInRows(start, end));
+                    threads[i].Start();
+                }
+            }
+
+            foreach (Thread th in threads)
+                th.Join();
+
+            _sum = _partSum.Sum();
+
+            return _sum;          
+        }
+
+        private void AddInRows(int from, int to)
+        {           
+            for (int i = from; i < to; i++)
+            {
+                _partSum[i] = 0;
+                for (int j = 0; j < Colums; j++)
+                {
+                    _partSum[i] += Value[i, j];
+                }
+            }
+        }
+
+        private int[,] GetMatrix(int n, int m)
+        {
+            Random rnd = new Random();
+            int[,] matrix = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = rnd.Next(0, 1000);
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ threads/Program.cs
@@ -0,0 +1,52 @@
+﻿using NLog;
+using printer;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsolePrinter printer = new ConsolePrinter();
+            Logger logger = LogManager.GetCurrentClassLogger();
+
+            int rows = 0, columns = 0, amountOfThreads = 1;
+
+            try
+            {
+                printer.WriteLine("Enter amount of rows:");
+                rows = Int32.Parse(printer.ReadLine());
+
+                printer.WriteLine("Enter amount of columns:");
+                columns = Int32.Parse(printer.ReadLine());
+
+                printer.WriteLine("Enter amount of threads:");
+                amountOfThreads = Int32.Parse(printer.ReadLine());
+            }
+            catch(FormatException e)
+            {
+                logger.Fatal("ReadLile" + e.Message);
+            }
+            
+            Matrix matrix = new Matrix(rows, columns);            
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            int sum = matrix.AddElements(amountOfThreads);
+
+            sw.Stop();
+
+            printer.WriteLine($"Sum = {sum}\nAmount of Threads = {amountOfThreads}, Elapsed = {sw.ElapsedMilliseconds} milliseconds");
+
+            printer.ReadLine();
+        }
+    }
+}
